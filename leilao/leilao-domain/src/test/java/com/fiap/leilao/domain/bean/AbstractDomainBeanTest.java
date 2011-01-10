e141556f3bcd2fb2f0153c2def458b4c6442bd5a
leandro1604 <leandro1604@e8d0c38a-1008-c7dd-2ac5-0770857e23c9>
2011-01-10 01:40:46 +0000

    desenvolvimento da camada de repository

---
+++ leilao/leilao-domain/src/test/java/com/fiap/leilao/domain/bean/AbstractDomainBeanTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.fiap.leilao.domain.bean;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Leandro
+ *
+ */
+public class AbstractDomainBeanTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.fiap.leilao.domain.bean.AbstractDomainBean#insert(com.fiap.leilao.domain.EntityBasic)}.
+	 */
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.fiap.leilao.domain.bean.AbstractDomainBean#delete(com.fiap.leilao.domain.EntityBasic)}.
+	 */
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.fiap.leilao.domain.bean.AbstractDomainBean#find(com.fiap.leilao.domain.EntityBasic)}.
+	 */
+	@Test
+	public void testFind() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.fiap.leilao.domain.bean.AbstractDomainBean#update(com.fiap.leilao.domain.EntityBasic)}.
+	 */
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+}
